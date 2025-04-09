26061f1642e151a2763d5b977c2abcd3aa8bfe16
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-04-09 21:11:26 +0530

    sdfg

---
+++ src/components/DisplayUsers.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+const DisplayUsers = () => {
+
+    const data = useSelector((state)=>{
+        return state.users
+    })
+
+    console.log(data)
+  return (
+    <Wrapper>
+        {
+            data.map((ele,id)=> {
+                return <li key={id}>
+                    {ele}
+                </li>
+            })
+        }
+    
+    </Wrapper>
+      
+    
+  )
+}
+
+const Wrapper = styled.section`
+    li{
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    padding:1rem;
+    border-bottom:1px solid #eee;
+
+    &:first-child{
+        border-top:1px solid #eee;
+    }
+    }
+`;
+
+
+export default DisplayUsers
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {Provider} from 'react-redux'
+import store from './store/slices/index.jsx'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+
+    <App />
+  </Provider>
+ 
+)
